e046555190042eb391ca104160fa36a92f5ace80
Giiwy <gestionnorec@gmail.com>
2025-06-13 00:16:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 
 fetch('carreras.json')
-  .then(response => response.json())
-  .then(carreras => {
-    const contenedor = document.getElementById('contenedor-carreras');
-    carreras.forEach(carrera => {
-      const tarjeta = document.createElement('div');
-      tarjeta.className = 'card';
-      tarjeta.innerHTML = `
-        <h2>${carrera.nombre}</h2>
-        <div class="info">📅 ${new Date(carrera.fecha).toLocaleDateString('es-ES')}</div>
-        <div class="info">📍 ${carrera.lugar}</div>
-        <div class="info">🚴 Categoría: ${carrera.categoria} · ${carrera.tipo} · ${carrera.distancia}</div>
-        <div class="link"><a href="${carrera.web}" target="_blank">Ir a la web ↗</a></div>
-      `;
-      contenedor.appendChild(tarjeta);
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error(err));;
     });
   })
   .catch(error => {
