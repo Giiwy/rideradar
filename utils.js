0d531c87d1d038f898beeb7f4882bee69c1c3eb2
Giiwy <gestionnorec@gmail.com>
2025-08-13 23:57:55 +0200

    Add files via upload

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ function eventoHTML(e) {
       <p><strong>Fecha:</strong> ${e.fecha}</p>
       <p><strong>Ubicación:</strong> ${e.ubicacion} (${e.provincia})</p>
       <p><strong>Desnivel:</strong> ${e.desnivel} m</p>
-      <a href="${e.enlace}" target="_blank">Más info</a>
+      <a href="detalle.html?id=${e.id}">Ver detalles</a>
     </div>
   `;
 }
